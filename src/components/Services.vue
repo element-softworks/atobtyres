78a951714d7e93f8c05687ce79959e8cf99a0425
Joe Methven <contact@joemethven.com>
2019-12-24 13:11:02 +0000

    Putting markup back

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -1,13 +1,13 @@
 <template>
 	<div v-if="type">
 		<div :class="`mb-4 card service-${type}`">
-<!--			<div class="card-header"></div>-->
-<!--			<div class="card-body">-->
-<!--				<h3><span class="marks">//</span>{{title}}</h3>-->
-<!--				<p class="pt-2">-->
-<!--					{{text}}-->
-<!--				</p>-->
-<!--			</div>-->
+			<div class="card-header"></div>
+			<div class="card-body">
+				<h3><span class="marks">//</span>{{title}}</h3>
+				<p class="pt-2">
+					{{text}}
+				</p>
+			</div>
 		</div>
 	</div>
 </template>
